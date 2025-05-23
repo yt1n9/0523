2c8a67dbccb39009b19071b620f80187e37f6f29
yt1n9 <a0905070101@gmail.com>
2025-05-23 10:16:03 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,41 @@
+let video;
+let facemesh;
+let predictions = [];
+const points = [409,270,269,267,0,37,39,40,185,61,146,91,181,84,17,314,405,321,375,291];
+ㄋ
+function setup() {
+  createCanvas(640, 480).position(
+    (windowWidth - 640) / 2,
+    (windowHeight - 480) / 2
+  );
+  video = createCapture(VIDEO);
+  video.size(width, height);
+  video.hide();
+
+  facemesh = ml5.facemesh(video, modelReady);
+  facemesh.on('predict', results => {
+    predictions = results;
+  });
+}
+
+function modelReady() {
+  console.log('Facemesh model loaded!');
+}
+
+function draw() {
+  image(video, 0, 0, width, height);
+
+  if (predictions.length > 0) {
+    let keypoints = predictions[0].scaledMesh;
+    stroke(255, 0, 0);
+    strokeWeight(15);
+    noFill();
+    beginShape();
+    for (let i = 0; i < points.length; i++) {
+      let idx = points[i];
+      let [x, y] = keypoints[idx];
+      vertex(x, y);
+    }
+    endShape();
+  }
+}
